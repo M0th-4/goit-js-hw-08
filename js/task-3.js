b2350e95d15135b475d0e9f912da70b37b7adb5e
M0th-4 <qwas5624@gmail.com>
2025-05-20 18:56:22 +0300

    home8

--- js/task-3.js
+++
@@ -1,6 +0,0 @@
-const textInput = document.querySelector('input#name-input');
-const textOutput = document.querySelector('span#name-output');
-
-textInput.addEventListener("input", () => {
-    const trimmedValue = textInput.value.trim();
-    textOutput.textContent = trimmedValue === "" ? "Anonymous" : trimmedValue})
